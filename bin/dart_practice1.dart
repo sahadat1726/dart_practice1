f35e0f64aebe9a268df53c5ad66207b81c377f87
sahadat1726 <sahadat1244m@gmail.com>
2024-09-24 03:52:29 +0600

    Initial commit

---
+++ bin/dart_practice1.dart
@@ -0,0 +1,15 @@
+void main(){
+
+  String title = 'True Love';
+  String author = 'Sohidul Islam';
+  int pub_year = 2024;
+
+
+
+
+  print('Book Name: $title');
+  print('Author Name: $author');
+  print('Publication Year: $pub_year');
+}
+
+
